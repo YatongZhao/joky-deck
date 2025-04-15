2395c7d9bb22fa6562d358696fccf45450a46ea7
yatongzhao <zyt768295828@163.com>
2025-04-15 23:29:56 +0800

    feat: refactor DevJokers component to separate JokerRow for improved organization and render joker cards in rows of ten

--- app/widgets/DevJokers.tsx
+++ app/widgets/DevJokers.tsx
@@ -1,26 +1,41 @@
-import { Box } from "@mantine/core";
+import { Box, Stack } from "@mantine/core";
 import { JokerCardWidget } from "./JokerCardWidget";
-import { Config, Game, Value } from "@yatongzhao/joky-deck-core";
+import { Config, Game, JokerCard, Value } from "@yatongzhao/joky-deck-core";
 import { useMemo } from "react";
 import { useDraggableCards } from "./DraggableCardList";
+import { splitEvery } from "ramda";
 
-export const DevJokers = ({ config }: { config: Config }) => {
+export const JokerRow = ({ jokers }: { jokers: JokerCard[] }) => {
   const jokers$ = useMemo(() => {
-    const game = new Game(config);
-    const jokers = config.jokers.map(joker => {
-      return joker.creator(game);
-    });
     return new Value(jokers);
-  }, [config]);
+  }, [jokers]);
 
   const { handleDrag, positionSignalMap } = useDraggableCards({
     cards$: jokers$,
     gap: 120,
   });
 
-  return <Box pos="relative" left={-300}>
-    {jokers$.value.map((joker, i) => (
+  return <Box>
+    {jokers.map((joker, i) => (
       <JokerCardWidget key={joker.id} joker={joker} positionSignal={positionSignalMap[joker.id]} onDrag={(props) => handleDrag(i, props)} />
     ))}
   </Box>
+}
+
+export const DevJokers = ({ config }: { config: Config }) => {
+  const jokerRows = useMemo(() => {
+    const game = new Game(config);
+    const jokers = config.jokers.map(joker => {
+      return joker.creator(game);
+    });
+    return splitEvery(10, jokers);
+  }, [config]);
+
+  return <Stack pos="relative" left={-295}>
+    {jokerRows.map((jokerRow, i) => (
+      <Box key={i} h={80}>
+        <JokerRow jokers={jokerRow} />
+      </Box>
+    ))}
+  </Stack>
 }
