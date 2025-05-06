e2e8cee9e54d1a9d7ec23951464263f923c88b4c
yatongzhao <zyt768295828@163.com>
2025-05-06 21:58:43 +0800

    feat: enhance ExportViewBoxController to manage viewBox transformations with deltaMatrix; improve cursor styling based on viewport settings for better user interaction

--- app/gear/ExportViewBoxController.tsx
+++ app/gear/ExportViewBoxController.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useRef } from "react";
-import { viewBoxA$, viewBoxB$ } from "./store";
+import { useEffect } from "react";
+import { finalMatrix$, viewBoxA$, viewBoxB$ } from "./store";
 import { combineLatest } from "rxjs";
+import { EditorMachineContext } from "./editorMachine";
+import { useDrag } from "./hooks/useDrag";
+import { vec2, mat3 } from "gl-matrix";
 
 export const ExportViewBoxController = ({ id }: { id?: string }) => {
-  const ref = useRef<SVGPathElement>(null);
+  const state = EditorMachineContext.useSelector((state) => state);
+  const isViewportSetting = state.matches("ViewportSetting");
+  const { ref, deltaMatrix$ } = useDrag<SVGPathElement>();
   
   useEffect(() => {
     const subscription = combineLatest([viewBoxA$, viewBoxB$]).subscribe(([a, b]) => {
@@ -12,14 +17,35 @@ export const ExportViewBoxController = ({ id }: { id?: string }) => {
       }
     });
     return () => subscription.unsubscribe();
-  }, []);
+  }, [ref]);
+
+  useEffect(() => {
+    const subscription = deltaMatrix$.subscribe((deltaMatrix) => {
+      const screenPositionA = vec2.create();
+      vec2.transformMat3(screenPositionA, viewBoxA$.getValue(), finalMatrix$.getValue());
+      vec2.transformMat3(screenPositionA, screenPositionA, deltaMatrix);
+      vec2.transformMat3(viewBoxA$.getValue(), screenPositionA, mat3.invert(mat3.create(), finalMatrix$.getValue()));
+      viewBoxA$.next(viewBoxA$.getValue());
+      
+      const screenPositionB = vec2.create();
+      vec2.transformMat3(screenPositionB, viewBoxB$.getValue(), finalMatrix$.getValue());
+      vec2.transformMat3(screenPositionB, screenPositionB, deltaMatrix);
+      vec2.transformMat3(viewBoxB$.getValue(), screenPositionB, mat3.invert(mat3.create(), finalMatrix$.getValue()));
+      viewBoxB$.next(viewBoxB$.getValue());
+    });
+    return () => subscription.unsubscribe();
+  }, [deltaMatrix$]);
+
   return (
     <path
       id={id}
+      style={{
+        cursor: isViewportSetting ? "grab" : "default",
+      }}
       ref={ref}
       stroke="none"
       strokeWidth="1"
-      fill="white"
+      fill="rgba(255, 255, 255, 1)"
       filter="drop-shadow(0 0 10px rgba(0, 0, 0, 0.1))"
     />
   )
