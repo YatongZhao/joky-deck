9d1f7abcf8e8c8b6debe15224175959d4008d02d
yatongzhao <zyt768295828@163.com>
2025-05-11 12:12:01 +0800

    feat: add vconsole for enhanced debugging in development; initialize vconsole in GearProject component to assist with console logging

--- app/gear/page.tsx
+++ app/gear/page.tsx
@@ -1,8 +1,18 @@
 "use client"
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import VConsole from "vconsole";
 
 const GearProject = dynamic(() => import("./GearProject").then(mod => mod.GearProject), { ssr: false });
 
 export default function Home() {
+  useEffect(() => {
+    // if (process.env.NODE_ENV === 'development') {
+      const vConsole = new VConsole();
+      return () => {
+        vConsole.destroy();
+      }
+    // }
+  }, []);
   return <GearProject />
 }
