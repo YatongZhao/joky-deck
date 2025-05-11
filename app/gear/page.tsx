a6ff85cb65ac9274d6861a9d027bcd994d2f756d
yatongzhao <zyt768295828@163.com>
2025-05-11 12:16:24 +0800

    refactor: simplify VConsole initialization by removing useEffect; directly instantiate VConsole in the GearProject component for improved debugging

--- app/gear/page.tsx
+++ app/gear/page.tsx
@@ -1,18 +1,11 @@
 "use client"
 import dynamic from "next/dynamic";
-import { useEffect } from "react";
 import VConsole from "vconsole";
 
 const GearProject = dynamic(() => import("./GearProject").then(mod => mod.GearProject), { ssr: false });
 
+new VConsole();
+
 export default function Home() {
-  useEffect(() => {
-    // if (process.env.NODE_ENV === 'development') {
-      const vConsole = new VConsole();
-      return () => {
-        vConsole.destroy();
-      }
-    // }
-  }, []);
   return <GearProject />
 }
